663755a3717c604be7749980eac78aaeda67755d
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-01-09 14:42:28 -0300

    feature/acesso-backend-e-adicionando-bindings

--- fifa_album_flutter/lib/fifa_world_app.dart
+++ fifa_album_flutter/lib/fifa_world_app.dart
@@ -1,24 +1,29 @@
+import 'package:fifa_album_flutter/core/rest/custom_dio.dart';
 import 'package:fifa_album_flutter/core/ui/theme/theme_config.dart';
 import 'package:fifa_album_flutter/pages/auth/auth/login/login_page.dart';
 import 'package:fifa_album_flutter/pages/home/home_page.dart';
-import 'package:fifa_album_flutter/pages/pages.dart';
 import 'package:fifa_album_flutter/pages/splash/splash_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 
 class FifaWorldApp extends StatelessWidget {
   const FifaWorldApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fifa World Cup Album',
-      theme: ThemeConfig.theme,
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (_) => const SplashRoute(),
-        '/auth/login': (_) => const LoginPage(),
-        '/home': (_) => const HomePage(),
-      },
+    return FlutterGetItApplicationBinding(
+      bindingsBuilder: () =>
+          [Bind.lazySingleton<CustomDio>((i) => CustomDio())],
+      child: MaterialApp(
+        title: 'Fifa World Cup Album',
+        theme: ThemeConfig.theme,
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (_) => const SplashRoute(),
+          '/auth/login': (_) => const LoginPage(),
+          '/home': (_) => const HomePage(),
+        },
+      ),
     );
   }
 }
--- fifa_album_flutter/lib/main.dart
+++ fifa_album_flutter/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:fifa_album_flutter/core/config/env/env.dart';
 import 'package:fifa_album_flutter/fifa_world_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Env.i.load();
   runApp(const FifaWorldApp());
 }
