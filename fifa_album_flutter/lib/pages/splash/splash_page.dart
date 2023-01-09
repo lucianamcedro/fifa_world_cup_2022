20920b3655f702c6ee1be4ffb5c9b21eb0175705
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-01-09 11:33:09 -0300

    feature/criação-helpers-loader-e-messages

--- fifa_album_flutter/lib/pages/splash/splash_page.dart
+++ fifa_album_flutter/lib/pages/splash/splash_page.dart
@@ -1,8 +1,16 @@
+import 'package:fifa_album_flutter/core/ui/helpers/loader.dart';
+import 'package:fifa_album_flutter/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
